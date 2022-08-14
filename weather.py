560945205ae0719bb52f50e301f20595786c20a0
noahmcatee <noahmcatee@gmail.com>
2022-08-14 09:53:26 -0700

    updating weather code

--- weather.py
+++ weather.py
@@ -12,7 +12,7 @@ if weather_API.status_code == 200:
     print("Website query successful. Status code", weather_API.status_code)
 
 json = weather_API.json()
-weather = json["properties"]["periods"]
+# weather = json["properties"]["periods"]
 # print(weather)
 temperature = json["properties"]["periods"][0]["temperature"]
 print("The temperature in Maricopa is", temperature)
@@ -20,6 +20,3 @@ forecast = json["properties"]["periods"][0]["shortForecast"]
 print("The forcast is", forecast)
 data_time = json["properties"]["periods"][0]["startTime"]
 print("This data was queried for", data_time)
-
-#tempurature = json.get("periods")[0].get("temperature")
-#print(tempurature)
