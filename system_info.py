76bf83a3e107537b43a2976871d62ce498f28343
noahmcatee <noahmcatee@gmail.com>
2022-08-14 19:26:29 -0700

    Updating sys info file

--- system_info.py
+++ system_info.py
@@ -2,15 +2,14 @@ from platform import platform
 import psutil
 import platform
 
-print(platform.node())
-print(platform.machine())
-print(platform.processor())
-print(platform.platform())
-print(platform.system())
-print(platform.release())
-print(platform.version())
+print("Device name:", platform.node())
+print("Device type:", platform.machine())
+print("Device processer:", platform.processor())
+print("Device platform:", platform.platform())
+print("Device OS:", platform.system())
+print("Device OS release:", platform.release())
+print("Device OS version:", platform.version())
 
 print("Disk Usage: ",psutil.disk_usage('/'))
 print("Virtual Mem: ",psutil.virtual_memory())
 print("CPU percent, last 10 seconds: ",psutil.cpu_percent(interval=10))
-
