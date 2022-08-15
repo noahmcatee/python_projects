d465adf1ac9b2c84557ccabaf17eab5e1b5079fe
noahmcatee <noahmcatee@gmail.com>
2022-08-14 19:37:33 -0700

    Updating Disney Game

--- disney_quiz_game.py
+++ disney_quiz_game.py
@@ -14,43 +14,43 @@ if answer.lower() == "1993":
     print("Correct!")
     score += 1
 else:
-    print("Incorrect!")
+    print("Nope. It was released in 1993.")
 
 answer = input("Who is the protaganist in Luca? ")
 if answer.lower() == "luca":
-    print("Correct!")
+    print("You got it!")
     score += 1
 else:
-    print("Incorrect!")
+    print("Nope. The protaganist is Luca.")
 
 answer = input("What do the pizza planet aliens see as their leader? ")
 if answer.lower() == "the claw":
-    print("Correct!")
+    print("That's it!")
     score += 1
 else:
-    print("Incorrect!")
+    print("Nope. The Claw is their leader.")
 
 answer = input("What does Sully use to calm Boo down? ")
 if answer.lower() == "little mikey":
-    print("Correct!")
+    print("Snailed it!")
     score += 1
 else:
-    print("Incorrect!")
+    print("Incorrect. He uses Little Mikey, the stuffed toy.")
 
 answer = input("Who does Andy give Woody to when he goes to College? ")
 if answer.lower() == "bonnie":
     print("Correct!")
     score += 1
 else:
-    print("Incorrect!")
+    print("Nope. He gives Andy to Bonnie when he leaves.")
 
 answer = input("What color shirt does Huey wear? ")
 if answer.lower() == "red":
-    print("Correct!")
+    print("Great job!")
     score += 1
 else:
-    print("Incorrect!")
+    print("Incorrect. Huey wears a red shirt.")
 
 print("You got " + str(score) + " questions correct!")
 print("You got " + str((score/6) * 100) + "%!")
-print("Great job! Thanks for playing!")
+print("Thanks for playing!")
