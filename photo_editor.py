4684a9c52a949c0f234b3fe0ae6ea70982797349
noahmcatee <noahmcatee@gmail.com>
2023-01-29 13:40:46 -0700

    photo editor changes

--- photo_editor.py
+++ photo_editor.py
@@ -1,5 +1,19 @@
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image, ImageEnhance, ImageFilter, ImageDraw, ImageFont
 import os
 
-path = 'C:\Users\noahm\Pictures\pyImages'
-pathOut = "C:\Users\noahm\Pictures\modPyImages"
+path = 'C:\\Users\\noahm\\Pictures\\pyImages'
+pathOut = "C:\\Users\\noahm\\Pictures\\modPyImages"
+
+text_font = (ImageFont.FreeTypeFont)
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}\{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN).convert('L')
+    
+    text_edit = ImageDraw.Draw(edit)
+    text_edit.text((100, 100), "Bob Burger", ("black"))
+
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'{pathOut}\{clean_name}_edited.jpg')
